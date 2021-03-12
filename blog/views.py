9d28d61c8404e475f83af36117877343abfb0cca
veluminous <sungyoon606@naver.com>
2021-03-13 03:42:55 +0900

    Add a function and page of creating post

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
+from django.shortcuts import redirect
 
 
 # Create your views here.
@@ -12,3 +14,17 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)  # commit=False: 바로 Post 모델에 저장하지 말 것 지시
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
